557431ec2f999e6d89816a87d0eecacaf85c814e
Kalaivanan Muthusamy <kalaivanan.muthusamy97@gmail.com>
2021-05-25 01:04:52 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+import crypto from 'crypto'
+
+const baseurl = "https://api.coindcx.com"
+const timeStamp = Math.floor(Date.now());
 
 function App() {
+
+  useEffect(() => {
+    getUserDetails();
+  }, [])
+
+  async function getUserDetails() {
+    console.log(timeStamp);
+    let body = {
+      "timestamp": timeStamp
+    }
+    const payload = new Buffer(JSON.stringify(body)).toString();
+    const signature = crypto.createHmac('sha256', process.env.REACT_APP_COINDCX_SECRET).update(payload).digest('hex')
+    const options = {
+      url: baseurl + "/exchange/v1/funding/fetch_orders",
+      headers: {
+        'X-AUTH-APIKEY': process.env.REACT_APP_COINDCX_KEY,
+        'X-AUTH-SIGNATURE': signature
+      },
+      json: true,
+      body: body
+    }
+    const res = await axios(options);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
